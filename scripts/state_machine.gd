bd6e253a78d481a069b8c46526e571d09cde55f2
Franklin Cruz <fcruz@smartboxtv.com>
2018-10-19 14:45:57 -0300

    Validate that new_state is not the same as current
    If both states are the same then we should not change

--- scripts/state_machine.gd
+++ scripts/state_machine.gd
@@ -2,18 +2,21 @@ extends Node2D
 
 var current_state = null
 
-func change_state(new_state): 
-    
+func change_state(new_state):
+
     if not new_state is State:
         printerr("new_state must inherit from State")
         return
-   
+
+    if new_state == self.current_state:
+        return
+
     if current_state != null:
         current_state.on_exit()
-        
+
     new_state.on_enter()
     current_state = new_state
-    
+
 func _process(delta):
     if current_state != null:
         current_state.step(delta)
@@ -28,7 +31,7 @@ class State:
 
     func _init(parent):
         self.parent = parent
-        
+
     func on_enter():
         pass
     func step(delta):
