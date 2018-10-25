1ad6a6e8d54f28b777b09afb614f339fdda9d697
Franklin Cruz <fcruz@smartboxtv.com>
2018-10-25 17:04:00 -0300

    Redraw pipeshaft when height changes

--- scenes/world/platforms/Pipe.gd
+++ scenes/world/platforms/Pipe.gd
@@ -2,7 +2,7 @@ tool
 extends Node2D
 
 # Height without the opening
-export(int) var height = 1
+export(int) var height = 1 setget _set_height
 
 onready var tilemap = $TileMap
 
@@ -11,6 +11,9 @@ var left_tile
 var right_tile
 
 func _ready():
+    self.draw_shaft()
+
+func draw_shaft():
     self.tileset = tilemap.get_tileset()
     self.left_tile = self.tileset.find_tile_by_name("pipes_foliage26")
     self.right_tile = self.tileset.find_tile_by_name("pipes_foliage27")
@@ -18,22 +21,8 @@ func _ready():
         self.tilemap.set_cellv(Vector2(-1, i), self.left_tile)
         self.tilemap.set_cellv(Vector2(0, i), self.right_tile)
 
-# func _process(delta):
-#    # Called every frame. Delta is time since last frame.
-#    # Update game logic here.
-#    pass
+func _set_height(new_height):
+    height = new_height
+    if Engine.is_editor_hint():
+        self.draw_shaft()
 
-"""
-extends TileMap
-
-var _tileset
-
-func _ready():
-    _tileset = get_tileset()
-    set_process_input(true)
-
-func _input(event):
-    var tile_pos = world_to_map(event.pos)
-    if event.is_action_pressed("add_tower"):
-        set_cellv(tile_pos, _tileset.find_tile_by_name("FireTower"))
-"""
