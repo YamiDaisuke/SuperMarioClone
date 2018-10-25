21d07aaf44e1118d50d7e9f27b1105c3a2bf0611
Franklin Cruz <fcruz@smartboxtv.com>
2018-10-25 16:58:13 -0300

    Refactor coroutines

--- scenes/world/FinishPole.gd
+++ scenes/world/FinishPole.gd
@@ -31,61 +31,60 @@ class Idle extends State:
 
     func _init(parent).(parent):
         self.name = "Idle"
-        
-        
+
+
 class Slide extends State:
-    
+
     func _init(parent).(parent):
         self.name = "Slide"
-        
+
     func on_enter():
         self.parent.player_ref.start_cinematic_cut()
         self.parent.player_ref.animation_player.current_animation = "PoleSlide"
         self.parent.player_ref.body.move_and_slide(Vector2(350,0))
-        
-    
+
+
     func on_exit():
         self.parent.player_ref.animation_player.stop()
         self.parent.player_ref.sprite.flip_h = true
         self.parent.player_ref.body.move_and_slide(Vector2(3000,0))
-    
-        
+
+
     func physics_step(delta):
         self.parent.player_ref.body.move_and_slide(Vector2(0,150))
-        
+
         if self.parent.player_ref.is_grounded():
             self.parent.change_state(self.parent.jumpoff_state)
-            
-            
+
+
 class JumpOff extends State:
-    
+
     func _init(parent).(parent):
         self.name = "Jump Off"
-        
+
     func on_enter():
         Coroutines.start(execute())
-    
+
     func execute():
         var player = self.parent.player_ref
         var jump_force = Vector2(75,-400)
         var velocity = Vector2(jump_force.x, jump_force.y)
-        
-        player.sprite.flip_h = false                
+
+        player.sprite.flip_h = false
         player.animation_player.current_animation = "Idle"
         yield(Coroutines.wait_for_seconds(0.1), "completed")
-        
+
         player.animation_player.current_animation = "Jump"
         var total_time = 0
         player.body.move_and_slide(velocity)
         yield(Coroutines.wait_for_frames(1), "completed")
 
         while not player.is_grounded():
-            total_time += Coroutines.physics_delta
+            total_time += Coroutines.get_physics_process_delta_time()
             velocity.y = jump_force.y + player.gravity * total_time
             player.body.move_and_slide(velocity)
             yield(Coroutines.wait_for_frames(1), "completed")
-        
-        player.animation_player.current_animation = "Idle"
-        self.parent.change_state(self.parent.idle_state)      
 
-        
+        player.animation_player.current_animation = "Idle"
+        self.parent.change_state(self.parent.idle_state)
+
