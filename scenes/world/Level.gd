8723a1f6fedb8ce6a680a2be8090d8fbe6b96424
Franklin Cruz <daisuke.sysoft@gmail.com>
2019-01-25 14:30:36 -0300

    Comment depracated code

--- scenes/world/Level.gd
+++ scenes/world/Level.gd
@@ -1,25 +1,36 @@
 extends Node
 
-export (int) var time = 100
+##########################
+##########################
+# DEPRECATED!!!!!
+##########################
+##########################
+##########################
 
-func _ready():
-    $HUD.set_time(self.time)
-    $HUD.set_score(0)
-    $HUD.set_world("0 - 0")
-    $HUD.set_player_name("FRANK")
-    $HUD.set_coins(0)
-    
-    $Timer.start()
+# export (int) var time = 100
 
-func _on_Player_died():
-    print("Game Over...")
-    get_tree().change_scene("res://scenes/game/GameOver.tscn")
+# func _ready():
+#     $HUD.set_time(self.time)
+#     $HUD.set_score(0)
+#     $HUD.set_world("0 - 0")
+#     $HUD.set_player_name("FRANK")
+#     $HUD.set_coins(0)
+
+#     $Timer.start()
+
+# func start():
+#     self.audio_player.play()
 
 
-func _on_Timer_timeout():
-    self.time -= 1
-    if self.time < 0:
-        return $Player.die()
-    
-    $HUD.set_time(self.time)
-    
+# func _on_Player_died():
+#     print("Game Over...")
+#     get_tree().change_scene("res://scenes/game/GameOver.tscn")
+
+
+# func _on_Timer_timeout():
+#     self.time -= 1
+#     if self.time < 0:
+#         return $Player.die()
+
+#     $HUD.set_time(self.time)
+
